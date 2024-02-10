e6dce75a65c914bc961858cdc920166cdce1e421
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2024-02-10 21:21:47 +0100

    created user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: [true, "First name is required"] },
+    lastName: { type: String, required: [true, "Last name is required"] },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      select: false,
+    },
+    refreshToken: { type: String, select: false },
+  },
+  { timestamps: true }
+);
 
 const UserModel = mongoose.model("User", UserSchema);
 
